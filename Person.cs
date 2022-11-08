224c6992362d25f08239c88d8a3de90b5a9247a9
shavar <shavar.litchmore@ecsazza.com>
2022-11-08 14:27:23 -0500

    first commit

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿
+namespace GitProfileBio { 
+    public class User {
+        private string name;
+        private string title;
+        private string interest;
+        private string courses;
+
+        public string Name { get => name; set => name = value; }
+        public string Title { get => title; set => title = value; }
+        public string Courses { get => courses; set => courses = value; }
+        public string Interest { get => interest; set => interest = value; }
+        public User() { Console.WriteLine("Please wait...generating profile bio.\n"); }
+        public override string ToString() => "Name: " + Name + "\nTitle: " + Title + "\nInterest: " + Interest + "\ncurrently learning: " + Courses; } }
+
+    
+
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+using GitProfileBio;
+
+namespace GithubProfile
+{
+    class Profile
+    {
+
+        public static void Main(string[] args)
+        {
+            var _description = "Shavar.,Software Engineer.,Augmented reality | Artificial Intelligence." +
+            ",C# | Ruby | Ruby | Go.";
+            var _profile = new Profile();
+            _profile.GreetNewVisitors();
+            Console.WriteLine(_profile.CreateBio(_description));
+
+        }
+        public User CreateBio(string _userData)
+        {
+            var _user = new User();
+            string[] userDetails = _userData.Split(",");
+            for (var i = 0; i < userDetails.Length; i++)
+            {
+                _user.Name = userDetails[0];
+                _user.Title = userDetails[1];
+                _user.Interest = userDetails[2];
+                _user.Courses = userDetails[3];
+            }
+            return _user;
+        }
+
+        public void GreetNewVisitors()
+        {
+            string msg = "";
+            Console.WriteLine("Welcome to my github profile! have you been here before?");
+            string returning = Console.ReadLine();
+            if (returning.Equals("yes", StringComparison.OrdinalIgnoreCase)) msg = "\nWelcome back!\n";
+            else msg = "Hello!, take a look around!\n";
+            Console.WriteLine(msg);
+        }
+    }
+}
+        
+            
+     
+   
+
+
